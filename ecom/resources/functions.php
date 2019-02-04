bc95a71dcb6cadd7de6bf120da6d11ff6efdd3db
t1 <ryanthonn@gmail.com>
2019-02-04 14:23:11 -0800

    displaying products and categories from db

--- ecom/resources/functions.php
+++ ecom/resources/functions.php
@@ -38,8 +38,42 @@ confirm($query);
 
 while($row = mysqli_fetch_array($query)){
 
-echo $row['product_price'];
+$products = <<<DELIMETER
+ <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <a href="item.php?={$row['product_id']}"><img src={$row['product_image']} alt=""></a>
+                            <div class="caption">
+                                <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+                                <h4><a href="product.html">{$row['product_title']}</a>
+                                </h4>
+                                <p>{$row['product_description']}</p>
+                            <a class="btn btn-primary" target="_blank" href="item.php?={$row['product_id']}">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+DELIMETER;
+
+echo $products;
+
+// echo $row['product_price'];
 }
+}
+
+function getCategories(){
+
+                    $theQuery = query("SELECT * FROM categories");
+
+                    confirm($theQuery);
+
+                    while($row = mysqli_fetch_array($theQuery)){
+
+$categories = <<<DELIMETER
+     <a href='category.php?id={$row['cat_id']}' class='list-group-item'> {$row['cat_title']} </a>
+DELIMETER;
+
+echo $categories;
+
+                    }
 }
 
 ?>
--- ecom/resources/templates/front/side-nav.php
+++ ecom/resources/templates/front/side-nav.php
@@ -2,18 +2,7 @@
                 <p class="lead">Shop Name</p>
                 <div class="list-group">
 
-                <?php
-
-
-                    $theQuery = query("SELECT * FROM categories");
-
-                    confirm($theQuery);
-
-                    while($row = mysqli_fetch_array($theQuery)){
-
-                    echo "<a href='' class='list-group-item'> {$row['cat_title']} </a>";
-
-                    }
+                <?php getCategories();
                 ?>
 
                     <a href="category.html" class="list-group-item">Category 1</a>
